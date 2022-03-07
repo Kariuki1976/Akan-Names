af15de24ae9b953effcdcd6a7145ccb34db1f604
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-03-07 10:04:33 -0500

    add logic to validate form for generating name

---
+++ Js folder/mains.js
@@ -0,0 +1,44 @@
+function submitForm(e) {
+    event.preventDefault();
+
+    let slim = document.getElementById('male')
+    let slimm = document.getElementById('female')
+
+    let nameManu = document.getElementById('here')
+
+
+
+
+
+
+
+
+    let dateEm = new Date(document.getElementById('date').value);
+
+    let male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+
+    let female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+
+
+    let finalManu = dateEm.getDay();
+
+
+    if (slim.checked == true) {
+        nameManu.innerText = "your name   is : " + (male[finalManu])
+
+
+    } else if (slimm.checked == true) {
+        nameManu.innerText = "your name   is : " + (female[finalManu])
+
+
+    }
+
+}
+
+// function clearData() {
+
+
+
+//     let dateEl = new Date(document.getElementById('date').value == null)
+
+// }
